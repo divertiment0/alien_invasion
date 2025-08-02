e8bc92442b6fa6012d525aa93903c4c76f94e9da
fenix <fenix@librebits.info>
2025-08-02 11:22:40 +0200

    $ python3 alien_invasion.py
    File "/home/fenix/Python-bits/Crash-course/alien_invasion/alien_invasion.py", line 34, in <module>
        ai.run_game()
        ^^^^^^^^^^^
    AttributeError: 'AlienInvasion' object has no attribute 'run_game'

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,18 +16,19 @@ class AlienInvasion:
      pygame.display.set_caption("Alien Invasion")
 
      def run_game(self):
-	 """ Start the main loop for the game."""
-	while True:
+        """ Start the main loop for the game."""
+        while True:
             # Watch for keyboard and mouse events.
-	    for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-		    sys.exit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
+
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
