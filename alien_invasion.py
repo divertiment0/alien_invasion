fdc78cca4c9d905a874b48471985e47b898300ea
fenix <fenix@librebits.info>
2025-08-02 18:26:41 +0200

    creating the game's Settings ii - pre create ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
  
     """ Overall class to manage game assets."""
@@ -12,8 +14,10 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(( 1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set background color.
@@ -28,7 +32,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
