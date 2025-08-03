673705c7900e1a019d4673f7e05a61d8d90100f7
fenix <fenix@librebits.info>
2025-08-03 18:14:54 +0200

    pre - drawing the ship to the screen

---
+++ ship.py
@@ -0,0 +1,22 @@
+import game
+
+class Ship:
+    """ A class to manage the ship."""
+
+    def _init(self, ai_game):
+        """ Initialize the ship"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the image an get it's rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship image and get its rect.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """ Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+        
