9f6f9db1cf893a83e3b3dc2f2809ddff5fbe0c67
fenix <fenix@librebits.info>
2025-08-12 16:02:22 +0200

    Adjusting the Ship's speed \!/ :
    Traceback (most recent call last):
      File "/home/fenix/Python-bits/Crash-course/alien_invasion/alien_invasion.py", line 69, in <module>
        ai.run_game()
      File "/home/fenix/Python-bits/Crash-course/alien_invasion/alien_invasion.py", line 32, in run_game
        self._update_screen()
      File "/home/fenix/Python-bits/Crash-course/alien_invasion/alien_invasion.py", line 57, in _update_screen
        self.ship.blitme()
      File "/home/fenix/Python-bits/Crash-course/alien_invasion/ship.py", line 39, in blitme
        self.screen.blit(self.image, self.rect)
        ^^^^^^^^^^^^^^^^
    AttributeError: 'Settings' object has no attribute 'blit'

--- ship.py
+++ ship.py
@@ -1,11 +1,14 @@
 import pygame
 
+# from settings import Settings
+
 class Ship:
     """ A class to manage the ship."""
 
     def __init__(self, ai_game):
         """ Initialize the ship"""
         self.screen = ai_game.screen
+        self.screen = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the image an get it's rect.
@@ -15,15 +18,20 @@ class Ship:
         # Start each new ship image and get its rect.
         self.rect.midbottom = self.screen_rect.midbottom
 
+
+        # Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+
+        
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """ Update the ship's position based on the movement flag. """
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         elif self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
 
 
     def blitme(self):
