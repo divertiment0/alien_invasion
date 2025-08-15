8711707d50f6cb420c8914f45562e845839350db
fenix <fenix@librebits.info>
2025-08-15 18:40:20 +0200

    post Pressing Q to Quit | pre Adding the Bullet Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,6 +19,14 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+
+        # Uncoment for fullscreen :
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
+
+        
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
